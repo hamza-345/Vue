a8ed1a03a846b51da8457ab2ce6d3e4b797dd754
Muhammad Hamza <hamza.hassan@mail.utoronto.ca>
2021-03-17 02:52:55 -0400

    initial files

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+ 
+    <div class="card container mt-3">
+    <div class="card-body">
+       <Header msg="Task Manager"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        <h2> {{msg}} </h2>
+        <button style = "color: green" class="btn btn-success">Add task</button>
+    </header>
+</template>
+
+<script>
+export default {
+    props: {
+        msg : String
+    }
+}
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+</style>
